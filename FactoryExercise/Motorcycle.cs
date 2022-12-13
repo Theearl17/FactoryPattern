d8c9612e67c6a2325c615ac72bf7e18e022a9feb
earl starks <earlstarks@rocketmail.com>
2022-12-13 16:13:24 -0600

    first commit

---
+++ FactoryExercise/Motorcycle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryExercise
+{
+    internal class Motorcycle : IVehicle
+    {
+        public Motorcycle() 
+        { 
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine(" The Motorcycle is revving up!");
+            Console.WriteLine("wom-wom-wom-wom!");
+        }
+    }
+}
---
+++ FactoryExercise/ThreeWheeler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryExercise
+{
+    internal class ThreeWheeler : IVehicle
+    {
+    public ThreeWheeler() 
+        { 
+        }
+    public void Drive () 
+        {
+            Console.WriteLine(" Let's beat the traffic!");
+        }
+    
+    }
+}
